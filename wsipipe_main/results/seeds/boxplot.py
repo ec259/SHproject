8f564aa6c04d9bc0c3a4613c384daef8946c9b74
ec259 <ec259@st-andrews.ac.uk>
2023-03-24 14:55:46 +0000

    Linegraph for final result

--- wsipipe_main/results/seeds/boxplot.py
+++ wsipipe_main/results/seeds/boxplot.py
@@ -27,29 +27,3 @@ plt.xlabel("Seed Number")
 plt.title("Validation Accuracies for Different Seeds")
 
 plt.savefig(fname="Seed_Experiment.png", dpi=350)
-
-# AUGMENTATIONS
-combos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-allCombosAllRuns = []
-
-for i in range(0, len(combos)):
-    allRunsForCombo = []
-    for j in range(0, 4):
-        data = read_csv("./augments_experiment/combo_" + str(combos[i]) + "_" + str(j))
-
-        valid_acc = data["valid_acc"].tolist()
-        allRunsForCombo.extend(valid_acc)
-
-    allCombosAllRuns.append(allRunsForCombo)
-
-fig = plt.figure()
-ax = plt.axes()
-bp = ax.boxplot(allCombosAllRuns)
-ax.set_ylim(0, 100)
-plt.yticks(np.arange(0, 101, 10))
-plt.xticks([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], combos)
-plt.ylabel("Validation Accuracy Percentage (%)")
-plt.xlabel("Combination Setting Number")
-plt.title("Validation Accuracies for Different Augmentation Combinations")
-
-plt.savefig(fname="Augments_Experiment.png", dpi=350)
