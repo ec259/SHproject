1486285d7968351ffbbc3e629535934bb5215fe5
ec259 <ec259@st-andrews.ac.uk>
2023-03-26 17:33:14 +0100

    Graph title rename

--- src/results/seeds/boxplot.py
+++ src/results/seeds/boxplot.py
@@ -7,15 +7,14 @@ seeds = [0, 42, 64, 123, 256, 301, 598, 647, 999, 1011]
 allSeedsAllRuns = []
 
 for i in range(0, len(seeds)):
-    topAccuraciesForSeed = []
+    accuraciesForSeed = []
     for j in range(0, 10):
         data = read_csv("seed_" + str(seeds[i]) + "_" + str(j))
 
         valid_acc = data["valid_acc"].tolist()
-        best = max(valid_acc)
-        topAccuraciesForSeed.append(best)
-    print(topAccuraciesForSeed)
-    allSeedsAllRuns.append(topAccuraciesForSeed)
+        # best = max(valid_acc)
+        accuraciesForSeed.extend(valid_acc)
+    allSeedsAllRuns.append(accuraciesForSeed)
 
 fig = plt.figure()
 ax = plt.axes()
@@ -25,6 +24,6 @@ plt.yticks(np.arange(0, 101, 10))
 plt.xticks([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], seeds)
 plt.ylabel("Validation Accuracy Percentage (%)")
 plt.xlabel("Seed Number")
-plt.title("Validation Accuracies for Different Seeds")
+plt.title("Validation Accuracies across all Epochs for Different Seeds (10 Runs)")
 
 plt.savefig(fname="Seed_Experiment.png", dpi=350)
